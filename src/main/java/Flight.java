16a0ea6b8fe59c6c85ed159bfe273fcdb3fe160e
Calum Cook <calumcook@Calums-Air.broadband>
2022-08-06 02:34:57 +0100

    flight

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Pilot> pilots;
+    private ArrayList<CabinCrew> cabinCrews;
+    private ArrayList<Passenger> bookedPassengers;
+    private PlaneType planeType;
+
+
+    public Flight() {
+        this.cabinCrews = new ArrayList<>();
+        this.bookedPassengers = new ArrayList<>();
+    }
+
+
 }
